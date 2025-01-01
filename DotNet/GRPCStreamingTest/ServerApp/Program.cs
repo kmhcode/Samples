9f5446a0483cb9628a70aba3dbf43cccf40029c7
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-01-01 09:01:53 -0500

    Add files via upload

--- DotNet/GRPCStreamingTest/ServerApp/Program.cs
+++ DotNet/GRPCStreamingTest/ServerApp/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using ServerApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
+builder.WebHost.ConfigureKestrel(options => 
+    options.ConfigureEndpointDefaults(settings => settings.Protocols = HttpProtocols.Http2)
+);
 var app = builder.Build();
 app.MapGrpcService<ComputeSupportService>();
-app.Run();
+app.Run("http://localhost:4040");
