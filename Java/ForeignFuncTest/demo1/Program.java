30a41c39dfba4c1abcd6264a8515c6f387cac8dc
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-02-24 08:05:27 -0500

    Add files via upload

--- Java/ForeignFuncTest/demo1/Program.java
+++ Java/ForeignFuncTest/demo1/Program.java
@@ -1,3 +1,6 @@
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
 
 class Program {
@@ -6,7 +9,9 @@ class Program {
         double p = Double.parseDouble(args[0]);
         int l = Integer.parseInt(args[1]);
         System.loadLibrary(args[2]);
-        var depreciationHandle = NativeHelper.importFunction(null, "depreciation", ValueLayout.JAVA_DOUBLE, ValueLayout.JAVA_INT, ValueLayout.JAVA_INT);
+        var depreciationHandle = Linker.nativeLinker().downcallHandle(
+            SymbolLookup.loaderLookup().find("depreciation").get(),
+            FunctionDescriptor.of(ValueLayout.JAVA_DOUBLE, ValueLayout.JAVA_INT, ValueLayout.JAVA_INT));
         for(int n = 1; n < l; ++n){
             double d = (double)depreciationHandle.invokeExact(l, n);
             System.out.printf("%d\t%.2f%n", n, p * (1 - d));
