3a8ca2c55621e1f6a3aa2e0089e7d99a0be38e97
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-02-26 08:02:36 -0500

    Add files via upload

---
+++ DotNet/PlatformInvokeTest/DemoApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+double cost = double.Parse(args[0]);
+int life = int.Parse(args[1]);
+nint assetLib = NativeLibrary.Load(args[2]);
+nint depreciationPtr = NativeLibrary.GetExport(assetLib, "depreciation");
+var depreciationStub = Marshal.GetDelegateForFunctionPointer<DepreciationFunc>(depreciationPtr);
+for(int after = 1; after < life; ++after)
+{
+    double d = depreciationStub.Invoke(life, after);
+    Console.WriteLine("{0}\t{1:0.00}", after, cost * (1 - d));
+}
+NativeLibrary.Free(assetLib);
