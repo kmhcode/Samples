598c51058da307482ffd3e0aff1f06e5e2da1d05
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-03-02 08:32:01 -0500

    Add files via upload

---
+++ DotNet/PlatformInvokeTest/DemoApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+class Program 
+{
+    delegate double DepreciationInvoker(int life, int after);
+
+    static void Main(string[] args)
+    {
+        double p = double.Parse(args[0]);
+        int l = int.Parse(args[1]);
+        nint assetLib = NativeLibrary.Load($"native/lib{args[2]}.so");
+        nint depreciationPtr = NativeLibrary.GetExport(assetLib, "depreciation");
+        var depreciationStub = Marshal.GetDelegateForFunctionPointer<DepreciationInvoker>(depreciationPtr);
+        for(int n = 1; n < l; ++n)
+        {
+            double d = depreciationStub.Invoke(l, n);
+            Console.WriteLine("{0}\t{1:0.00}", n, p * (1 - d));
+        }
+        NativeLibrary.Free(assetLib);
+    }
+}
---
+++ DotNet/PlatformInvokeTest/DemoApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+unsafe class Program
+{
+    [DllImport("native/libprimes.so", EntryPoint = "primes_fetch")]
+    static extern void FetchPrimes(ulong start, int count, delegate*<ulong, bool> selector, ulong* selected);
+
+    static bool IsFavorite(ulong num) => (num - 1) % 4 == 0;
+
+    static void Main(string[] args)
+    {
+        ulong m = ulong.Parse(args[0]);
+        if(args.Length == 1)
+        {
+            ulong prime = 0;
+            FetchPrimes(m, 1, null, &prime);
+            Console.WriteLine(prime);
+        }
+        else
+        {
+            int n = int.Parse(args[1]);
+            Span<ulong> primes = n > 8 ? new ulong[n] : stackalloc ulong[n];
+            fixed(ulong* store = &primes[0])
+            {
+                FetchPrimes(m, n, &IsFavorite, store);
+            }
+            foreach(ulong prime in primes)
+                Console.WriteLine(prime);
+        }
+    }
+}
