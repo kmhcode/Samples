b8172055712efc2c6ddf9c3e8a7f8607795d51bd
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-03-03 08:42:44 -0500

    Delete Native directory

--- Native/extproctest3.py
+++
@@ -1,8 +0,0 @@
-from ctypes import *
-
-dijkstra = CDLL("./libdijkstra.so")
-m = int(input("Numerator : "))
-n = int(input("Denominator: "))
-g = dijkstra.GCD(m, n)
-print("Reduced Fraction = %d/%d" % (m / g, n / g))
-
