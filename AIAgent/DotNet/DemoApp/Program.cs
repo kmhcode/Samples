b23f342ce9d5ec6eb72ff99e06e97d45887a992a
kmhcode <88838522+kmhcode@users.noreply.github.com>
2025-12-19 07:28:51 +0530

    Latest

---
+++ AIAgent/DotNet/DemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using DemoApp;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(
+    modelId: "default-llm",
+    endpoint: new Uri("http://iitdac.met.edu/openai/"),
+    apiKey: "not-required"
+);
+builder.Plugins.AddFromObject(new DiscountAgent());
+var kernel = builder.Build();
+var executionSettings = new OpenAIPromptExecutionSettings
+{
+    ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions,
+    Temperature = 0 //range: 0.0(pure logic) - 1.0(high creativity)
+};
+var chatHistory = new ChatHistory(args[0], AuthorRole.User);
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+var messageContent = await chatService.GetChatMessageContentAsync(chatHistory, executionSettings, kernel);
+var functionContent = messageContent.Items.OfType<FunctionCallContent>().FirstOrDefault();
+if(functionContent != null)
+{
+    var reply = await functionContent.InvokeAsync(kernel);
+    Console.WriteLine(reply.Result);
+}
+
+//dotnet run "What will be the discount for 12 apples"
---
+++ AIAgent/Java/demo/src/main/java/app/Program.java
@@ -0,0 +1,36 @@
+package app;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.Result;
+
+public class Program {
+    
+    interface Assistant {
+        Result<String> ask(String question);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("slf4j.internal.verbosity", "NONE");
+        var model = OpenAiChatModel.builder()
+            .modelName("default-llm")
+            .baseUrl("http://iitdac.met.edu/openai/")
+            .apiKey("not-required")
+			.temperature(0.0) //range: 0.0(pure logic) - 1.0(high creativity)
+            .build();
+        var chatService = AiServices.builder(Assistant.class)
+            .chatModel(model)
+            .tools(new DiscountAgent())
+            .build();
+        var toolResults = chatService.ask(args[0])
+            .toolExecutions();
+        if(toolResults.size() > 0){
+            var reply = toolResults.get(0);
+            System.out.println(reply.result());
+        }
+    }
+
+}
+
+
+//bash run "What will be the discount for 12 apples"
